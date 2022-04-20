bea07aee8e52e126f5fa0d729ef2847b452ae68f
Antonio <antonio.garciamn@gmail.com>
2022-04-20 13:33:23 -0300

    docs: incia docs configs

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import router from "./routers/index.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+
+app.listen(process.env.PORT, () =>
+	console.log(`Listen on port ${process.env.PORT}`)
+);
